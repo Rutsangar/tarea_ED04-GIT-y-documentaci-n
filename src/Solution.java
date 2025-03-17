994c4d831c782155d4465a043c29175191a3692b
Rutsangar <ruth.sanchez.garcia21@gmail.com>
2025-03-17 23:14:02 +0000

    documentado

--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,11 @@
 public class Solution {
     static String toCamelCase(String s){
+        /**
+         * Metodo que hace un camel case de un string separado por "-" como separador estos caracteres
+         * El metodo respeta la primera letra de la primera palabra, sea mayuscula o minuscula y convierte el resto de palabras
+         * @param s el parametro es un string
+         * @return devuelve el string convertido
+         */
         boolean espacio = false;
         String str = "";
 
@@ -17,4 +23,5 @@ public class Solution {
         }
         return str;
     }
+
 }
