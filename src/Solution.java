7c50fc29a71c8badbb8e767bf00b20a433a14bae
Rutsangar <ruth.sanchez.garcia21@gmail.com>
2025-03-17 22:58:25 +0000

    v2

--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,21 @@
 public class Solution {
     static String toCamelCase(String s){
-        return "";
+        boolean espacio = false;
+        String str = "";
+
+        for(char caracter : s.toCharArray()){
+            if(Character.isLetter(caracter)){
+                if(espacio == true){
+                    str += Character.toUpperCase(caracter);
+                    espacio = false;
+                } else {
+                    str += caracter;
+                }
+            } else {
+                espacio = true;
+            }
+        }
+
+        return str;
     }
 }
